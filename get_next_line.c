b52038a0878aa29bfcf6c417554ecc348d557821
musipit <muratdemions@gmail.com>
2026-02-18 11:53:34 +0300

    ADD_FUNC: gnl.c(appointer)

--- get_next_line.c
+++ get_next_line.c
@@ -6,6 +6,33 @@ static void *spt_free(void *freeable_area)
 	return (NULL);
 }
 
+static char *appointer(char *to_read)
+{
+	char		*line;
+	long long	i;
+
+	i = 0;
+	if(!to_read[i])
+		return (NULL);
+	while (to_read[i] != '\n' && to_read[i] != '\0')
+		i++;	
+	if(to_read[i] == '\n')
+		i++;
+	line = malloc(i + 1);
+	if(!line)
+		return (NULL);
+	i = 0;
+	while (to_read[i] != '\n' && to_read[i] != '\0')
+	{
+		line[i] = to_read[i];
+		i++;
+	}
+	if(to_read[i] == '\n')
+		line[i] = '\n';
+	line[i+1] = '\0';
+	return (line);
+
+}
 static void *spt_to_read(int fd, char *to_read)
 {
 	char		*txt;
@@ -44,5 +71,14 @@ char	*get_next_line(int fd)
 		return (NULL);
 	to_read = spt_to_read(fd, to_read);
 	if(!to_read)
-		return (NULL);	
+		return (NULL);
+	line = appointer(to_read);
+	if(!line)
+	{
+		free(to_read);
+		to_read = NULL;
+		return (NULL);
+	}
+	return(line);
+	
 }
